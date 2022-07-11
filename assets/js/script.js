a85d1f1acb81b86701b8ed0680a721304c5ad917
Gabriel <gabrielsouzapessoa@gmail.com>
2022-07-11 17:55:45 -0300

    added projects section

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,15 @@
 const hamburgerBtn = document.querySelector('.hamburger');
 const headerNav = document.querySelector('.header-nav');
 
-const handleHamburgerClick = () => {
+const handleHamburgerClick = (e) => {
   if (headerNav.classList.contains('active'))
     headerNav.classList.remove('active');
   else headerNav.classList.add('active');
 };
 
 hamburgerBtn.addEventListener('click', handleHamburgerClick);
+window.addEventListener('click', (e) => {
+  if (!headerNav.contains(e.target) && !hamburgerBtn.contains(e.target)) {
+    headerNav.classList.remove('active');
+  }
+});
