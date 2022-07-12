b87cb48a22246a888dd9c27d61cf9846faedd3e3
Gabriel <gabrielsouzapessoa@gmail.com>
2022-07-12 17:54:48 -0300

    added remaining sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,59 @@ window.addEventListener('click', (e) => {
     headerNav.classList.remove('active');
   }
 });
+
+// Scroll Top
+
+let btnScrollTop = $('.btn-scroll-top');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 600) {
+    btnScrollTop.addClass('active');
+  } else {
+    btnScrollTop.removeClass('active');
+  }
+});
+
+btnScrollTop.click(() => {
+  $('html, body').animate({ scrollTop: 0 }, 400);
+});
+
+// Smooth Anchor Scroll
+
+$(document).on('click', 'a[href^="#"]', function (e) {
+  var id = $(this).attr('href');
+
+  var $id = $(id);
+
+  if ($id.length === 0) {
+    return;
+  }
+
+  e.preventDefault();
+
+  var pos = $id.offset().top;
+
+  $('body, html').animate({ scrollTop: pos });
+});
+
+// WhatsApp Form
+
+function submitWhatsAppMessage() {
+  let nome = $('#nome').val(),
+    email = $('#email').val(),
+    nomeProjeto = $('#nomeProjeto').val(),
+    descProjeto = $('#descProjeto').val(),
+    btnEnviar = $('.btn-whatsapp');
+
+  let msgFinal =
+    window.encodeURIComponent(`Olá, me chamo *${nome}*, estou a procura de um desenvolvedor.
+
+  *• E-mail:* ${email}
+  *• Projeto:* ${nomeProjeto}
+  *• Descrição do Projeto:* ${descProjeto}`);
+
+  let url = `https://wa.me/5544998182849?text=${msgFinal}`;
+  console.log();
+  btnEnviar.attr('href', url);
+}
+
+$('.btn-whatsapp').on('click', submitWhatsAppMessage);
